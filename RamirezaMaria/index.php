05f689a65f77b270d912a2888ada0f4bc964f78b
ElProfe <educaysoft@gmail.com>
2023-03-29 13:55:36 -0500

    cree mi tarea b2

---
+++ RamirezaMaria/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>
+  Pagina de Ramirez Maria
+
+</title>
+</head>
+<body>
+    <div   style="border:2px red solid;  background-color:gray;font-size:40px">
+  HOJA DE VIDA
+</div>
+<div>
+   CUERPO DE LA HOJA DE VIDA
+</div>
+
+<div>
+   FOOTHER DE LA HOJA DE VIDA
+</div>
+
+</body>
+
+
+
+
+</html>
